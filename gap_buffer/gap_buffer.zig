d6db6cfa99d8ec71fbd3d43d50b8f361b805f36e
Logan Ferreira <starvingslvt@gmail.com>
2024-05-10 17:44:53 -0400

    Implement GapBuffer init

--- gap_buffer/gap_buffer.zig
+++ gap_buffer/gap_buffer.zig
@@ -9,12 +9,31 @@ pub fn GapBufferType() type {
         };
 
         allocator: std.mem.Allocator,
+        grow_size: usize,
         size: usize,
         gap_start: usize,
         gap_end: usize,
         buffer: []u8,
 
-        pub fn init(allocator: std.mem.Allocator) GapBuffer {}
+        pub fn init(initial_size: usize, grow_size: usize, allocator: std.mem.Allocator) !GapBuffer {
+            if (grow_size == 0) return GapBufferError.GrowSizeLessThanOne;
+
+            const true_initial_size = if (initial_size == 0) {
+                grow_size;
+            } else {
+                initial_size;
+            };
+
+            return GapBuffer {
+                .allocator = allocator,
+                .grow_size = grow_size,
+                .size = initial_size,
+                .gap_start = 0,
+                .gap_end = true_initial_size,
+                .buffer = try allocator.alloc(u8, true_initial_size),
+            };
+        }
+
         pub fn deinit(buffer: *GapBuffer) void {}
         pub fn insertChar(buffer: *GapBuffer, char: u8) void {}
         pub fn insertStr(buffer: *GapBuffer, str: []const u8) void {}
