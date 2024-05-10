ce4aa6efc8ec83174be298e171d39c0f938f1f50
Logan Ferreira <starvingslvt@gmail.com>
2024-05-10 15:58:42 -0400

    Implement initial GapBuffer type

---
+++ gap_buffer/gap_buffer.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+
+pub fn GapBufferType() type {
+    return struct {
+        const GapBuffer = @This();
+
+        allocator: std.mem.Allocator,
+        size: usize,
+        gap_start: usize,
+        gap_end: usize,
+        buffer: []u8,
+
+        pub fn init(allocator: std.mem.Allocator) GapBuffer {}
+        pub fn deinit(buffer: *GapBuffer) void {}
+        pub fn insertChar(buffer: *GapBuffer, char: u8) void {}
+        pub fn insertStr(buffer: *GapBuffer, str: []const u8) void {}
+        pub fn shiftBufferToPosition(buffer: *GapBuffer, position: usize) void {}
+        fn _grow(buffer: *GapBuffer) void {}
+    };
+}
