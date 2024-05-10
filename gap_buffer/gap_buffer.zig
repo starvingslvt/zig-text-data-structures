0625fdd60ee54d94a2b911a78e940744594d77e6
Logan Ferreira <starvingslvt@gmail.com>
2024-05-10 18:02:57 -0400

    Implement GapBuffer _grow

--- gap_buffer/gap_buffer.zig
+++ gap_buffer/gap_buffer.zig
@@ -41,6 +41,20 @@ pub fn GapBufferType() type {
         pub fn insertChar(buffer: *GapBuffer, char: u8) void {}
         pub fn insertStr(buffer: *GapBuffer, str: []const u8) void {}
         pub fn shiftBufferToPosition(buffer: *GapBuffer, position: usize) void {}
-        fn _grow(buffer: *GapBuffer) void {}
+
+        fn _grow(buffer: *GapBuffer) void {
+            const new_size = buffer.size + buffer.grow_size;
+            
+            buffer.allocator.resize(buffer.buffer, new_size) catch {
+                const new_buffer = try buffer.allocator.alloc(u8, new_size);
+                @memcpy(new_buffer[0..buffer.size], buffer.buffer[0..buffer.size]);
+
+                buffer.allocator.free(buffer.buffer);
+                buffer.buffer = new_buffer;
+            };
+
+            buffer.size = new_size;
+            buffer.gap_end += buffer.grow_size;
+        }
     };
 }
